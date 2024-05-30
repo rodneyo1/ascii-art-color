e2456666269c0169fee7cb0001265edc76fd660d
oumaphilip01@gmail.com <oumaphilip01@gmail.com>
2024-05-30 16:08:26 +0300

    final commit of asciart color

--- main/main.go
+++ main/main.go
@@ -12,22 +12,43 @@ import (
 
 func main() {
 	// output := flag.String("output", "banner.txt", "output file") // create an output flag
-	color := flag.String("color", "", "text color") // create color flag
+	color := flag.String("color", "", "text color")// create color flag
+	flag.Usage = func() { // define usage
+		fmt.Printf("Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <letters to be colored> \"something\"\n")
+		os.Exit(0)
+	}
 	flag.Parse()
+
+	for _, arg := range os.Args {
+		if arg[len(arg)-1] == '=' {
+			flag.Usage()
+		}
+		if arg[0] == '=' {
+			flag.Usage()
+		}
+		if arg == "--color" {
+			flag.Usage()
+		}
+		if arg == "=" {
+			flag.Usage()
+		}
+		if strings.HasPrefix(arg, "-") && !strings.HasPrefix(arg, "--") {
+			flag.Usage()
+		}
+	}
+	
 	var r string
 	var tocolor string
 	var file *os.File
 	var err error
-	
-	if len(os.Args) < 2 || len(os.Args) > 5 {
+	if len(os.Args) < 2 || len(os.Args) > 5  {
 		fmt.Print("Ooops! few or too many arguments. Usages are:\ngo run . [string]\ngo run . [color option] [string]\ngo run . [color option] [string] [banner]\ngo run . [color option] [string to color] [string]\ngo run . [color option] [string to color] [string] [banner]\n")
 		os.Exit(0)
-	}
+	} 
 	if len(os.Args) == 2 {
 		r = os.Args[1]
 		tocolor = ""
 		*color = ""
-
 	}
 	if len(os.Args) == 3 {
 		r = flag.Args()[0]
@@ -35,7 +56,7 @@ func main() {
 	}
 
 	if len(os.Args) == 5 {
-		file, err = os.Open(flag.Args()[2] + ".txt")
+		file, err = os.Open(flag.Args()[2]+".txt")
 		if err != nil {
 			fmt.Println("Unable to open file!")
 			os.Exit(0)
@@ -46,16 +67,16 @@ func main() {
 	} else {
 		file, err = os.Open("standard.txt")
 		if err != nil {
-			fmt.Println("Unable to open file!")
-			os.Exit(0)
+		fmt.Println("Unable to open file!")
+		os.Exit(0)
 		}
 		defer file.Close()
 	}
 
-	if len(os.Args) == 4 {
+	if len(os.Args) == 4 {	
 		if flag.Args()[1] == "shadow" || flag.Args()[len(flag.Args())-1] == "standard" || flag.Args()[len(flag.Args())-1] == "thinkertoy" {
 			r = flag.Args()[0]
-			file, err = os.Open(flag.Args()[1] + ".txt")
+			file, err = os.Open(flag.Args()[1]+".txt")
 			if err != nil {
 				fmt.Println("Unable to open file", flag.Args()[1]+".txt!")
 				os.Exit(0)
@@ -63,10 +84,10 @@ func main() {
 			defer file.Close()
 		} else {
 			r = flag.Args()[1]
-			tocolor = flag.Args()[0]
+			tocolor = flag.Args()[0]	
 		}
 	}
-
+	
 	scanner := bufio.NewScanner(file)
 	var lines []string
 
@@ -89,9 +110,10 @@ func main() {
 		character = lines[i:end]
 		result = append(result, character)
 	}
-
+	
 	// Format ("/n") in input string
 	s := strings.Replace(r, "\\n", "\n", -1)
 	s = strings.Replace(s, "\\t", "    ", -1)
 	asciiart.HandleLn(s, result, color, tocolor)
+	//fmt.Println("Art has been created successfully!")
 }
